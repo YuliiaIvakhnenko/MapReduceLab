e7c735d86d3d2b758da43ca4d767d0581ecd4052
IvakhnenkoYuliia <ivakhnenko.yuliia@lll.kpi.ua>
2025-11-10 10:54:30 +0200

    Implemented map and reduce methods

--- src/student_jobs/word_count/mapper.py
+++ src/student_jobs/word_count/mapper.py
@@ -1,8 +1,37 @@
+import re
 from src.core.job.mapper import Mapper
 
 
 class WordCountMapper(Mapper):
     def map(self, record, emit):
-        for token in str(record).split():
-            emit(token.lower(), 1)
+        text = str(record).lower()
+        words = re.findall(r"[a-zA-Zа-яА-ЯєЄіІїЇґҐ']+", text)
 
+
+        total_words = len(words)
+        long_words = sum(1 for w in words if len(w) > 5)
+        emit("total_words", total_words)
+        emit("long_words", long_words)
+
+
+        vowels_set = set("aeiouаеєиіїоуюя")
+
+
+        for word in words:
+            length = len(word)
+            if length == 0:
+                continue
+
+
+            v = sum(1 for c in word if c in vowels_set)
+            c = sum(1 for c in word if c.isalpha() and c not in vowels_set)
+            total_letters = v + c
+            if total_letters == 0:
+                continue
+
+
+            vowels_percent = (v / total_letters) * 100
+            consonants_percent = (c / total_letters) * 100
+
+
+            emit(f"{length}", (vowels_percent, consonants_percent))
--- src/student_jobs/word_count/reducer.py
+++ src/student_jobs/word_count/reducer.py
@@ -3,6 +3,16 @@ from src.core.job.reducer import Reducer
 
 class WordCountReducer(Reducer):
     def reduce(self, key, values, emit):
-        emit(key, sum(values))
+        if key in ("total_words", "long_words"):
+            emit(key, sum(values))
+        else:
 
 
+            total_v = sum(v for v, c in values)
+            total_c = sum(c for v, c in values)
+            count = len(values)
+            avg_v = round(total_v / count, 2) if count else 0
+            avg_c = round(total_c / count, 2) if count else 0
+
+
+            emit(key, f"vowels: {avg_v}%    consonants: {avg_c}%")
